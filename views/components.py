0855dcc5d9b1c92b251dca370a622ceb4a0c33e5
Antoine Grigis <antoine.grigis@cea.fr>
2014-10-17 09:55:23 +0200

    rql_download/views/components: add fine rules on the download/facet mechanism.

--- views/components.py
+++ views/components.py
@@ -10,8 +10,9 @@
 from cubicweb.predicates import nonempty_rset, anonymous_user
 from cubicweb.web import component
 from cubicweb.predicates import (
-    is_instance, nonempty_rset, anonymous_user)
-from cubicweb.web.views.facets import FilterBox, FacetFilterMixIn
+    is_instance, nonempty_rset, anonymous_user, non_final_entity)
+from cubicweb.web.views.facets import (
+    facets, FilterBox, FacetFilterMixIn, contextview_selector)
 from logilab.mtconverter import xml_escape
 from cubicweb.web.views.bookmark import BookmarksBox
 
@@ -23,17 +24,20 @@ RQL_DOWNLOAD_SEARCH_ENTITIES = ["Scan", "ProcessingRun", "Subject"]
 # Save CW search box
 ###############################################################################
 
-class SaveCWSearchBox(FacetFilterMixIn, component.CtxComponent):
+class SaveCWSearchFilterBox(FacetFilterMixIn, component.CtxComponent):
     """ Class that enables us to display a 'Save search' box when the selected
-    entities fulfill the '__selected__' requirements.
+    entities fulfill the '__select__' requirements.
+
+    The global parameter 'RQL_DOWNLOAD_SEARCH_ENTITIES' specify with entities
+    can be downloaded.
+    The filter items are found dynamically.
     """
-    __regid__ = "ctx-save-search-box"
-    __select__ = (component.CtxComponent.__select__ & nonempty_rset() &
-                  ~anonymous_user() & 
-                  is_instance(*RQL_DOWNLOAD_SEARCH_ENTITIES))
+    __regid__ = "facet.filterbox"
+    __select__ = ((non_final_entity() & nonempty_rset())
+                  | contextview_selector())
     context = "left"
     order = 0
-    title = _("Tools")
+    title = _("Filter")
 
     linkbox_template = u'<div class="cw_search_box">{0}</div>'
 
@@ -45,17 +49,35 @@ class SaveCWSearchBox(FacetFilterMixIn, component.CtxComponent):
             This method only consider the first registered 'ns-save-search'
             action to generate the new CWSearch form.
         """
+
         # Get the component context
         rset, vid, divid, paginate = self._get_context()
+
+        # Check if some facets are defined for this view
+        nb_facet_widgets = len(facets(self._cw, rset, self.__regid__)[1])
+
+        # Check if the view information can be downloaded
+        can_save_search = False
+        try:
+            entity_name = rset.entities().next().__class__.__name__
+            if entity_name in RQL_DOWNLOAD_SEARCH_ENTITIES:
+               can_save_search = True
+        except:
+            pass
+        # Can't download if not logged
+        can_save_search = (can_save_search and 
+                           not self._cw.session.anonymous_session)
 
         # Check we have a none empty rset and a valid vid
         if vid is None:
             vid = self._cw.form.get("vid")
 
         # Create the form url
-        w(self.search_link(rset))
-        self.generate_form(w, rset, divid, vid, paginate=paginate,
-                           hiddens={}, **self.cw_extra_kwargs)
+        if can_save_search:
+            w(self.search_link(rset))
+        if nb_facet_widgets > 0:
+            self.generate_form(w, rset, divid, vid, paginate=paginate,
+                               hiddens={}, **self.cw_extra_kwargs)
 
     def search_link(self, rset):
         """ Method that generates a the url of the CWSearch form we want to save.
@@ -93,13 +115,15 @@ class SaveCWSearchBox(FacetFilterMixIn, component.CtxComponent):
         return self.linkbox_template.format(button)
 
     def _get_context(self):
-        view = self.cw_extra_kwargs.get('view')
-        context = getattr(view, 'filter_box_context_info', lambda: None)()
+        """ Method to get the in context box information.
+        """
+        view = self.cw_extra_kwargs.get("view")
+        context = getattr(view, "filter_box_context_info", lambda: None)()
         if context:
             rset, vid, divid, paginate = context
         else:
             rset = self.cw_rset
-            vid, divid = None, 'pageContent'
+            vid, divid = None, "pageContent"
             paginate = view and view.paginable
         return rset, vid, divid, paginate
 
@@ -109,6 +133,7 @@ class SaveCWSearchBox(FacetFilterMixIn, component.CtxComponent):
 ###############################################################################
 
 def registration_callback(vreg):
-    vreg.register(SaveCWSearchBox)
     vreg.unregister(FilterBox)
     vreg.unregister(BookmarksBox)
+    vreg.register(SaveCWSearchFilterBox)
+
