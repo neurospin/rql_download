3b6fcd0150c2cb9819aee4026aca94c3130a2950
Antoine Grigis <antoine.grigis@cea.fr>
2015-12-08 17:46:17 +0100

    release 1.2.0.

--- rql_download/__pkginfo__.py
+++ rql_download/__pkginfo__.py
@@ -4,7 +4,7 @@
 modname = 'rql_download'
 distname = 'cubicweb-rql-download'
 
-numversion = (1, 0, 1)
+numversion = (1, 2, 0)
 version = '.'.join(str(num) for num in numversion)
 
 license = 'LGPL'
